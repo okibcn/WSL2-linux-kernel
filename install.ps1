4684f96264b0de9e01a4046a5a92409d6e8229cc
Mioki <22417711+okibcn@users.noreply.github.com>
2023-02-16 14:44:39 -0800

    1st test

--- install.ps1
+++ install.ps1
@@ -1,17 +1,32 @@
-cd $PSScriptRoot
-$kernel=(gci WSLk*).PSPath
-$line="kernel=$($kernel -replace '/|\\','\\')"
-$wslcfg="~/.wslconfig"
-cp $wslcfg $PSScriptRoot
-if ( (Test-Path $wslcfg) -AND (gc $wslcfg | sls "^ *kernel *=") ) {
-    # We already have a kernel setting
-    (gc $wslcfg) -Replace "^ *kernel *=.*","$line" | Set-Content $wslcfg
-} else {
-    if ( !( (Test-Path $wslcfg) -AND (gc $wslcfg | sls "^ *[WSL2]") ) ){
-        # We don't have a [WSL2] section
-        "`n[WSL2]" >> $wslcfg
-    } 
-    "'n$line" >> $wslcfg
-}
-echo " Configuration file $wslcfg has been modified. A backup of the old file 
+
+function install_krnl {
+    cd $PSScriptRoot
+    $kernel = (gci WSLk*).FullName
+    $newline = "kernel=$($kernel -replace '/|\\','\\')"
+    $wslcfg = "~/.wslconfig"
+    $WSL2line = $Krnlline = [int]0
+
+    if (Test-Path $wslcfg) {
+        $WSL2line = [int]((gc $wslcfg) | sls "^ *\[wsl2]").Linenumber
+        $Krnlline = [int]((gc $wslcfg) | sls "^ *kernel *=").Linenumber
+        cp $wslcfg $PSScriptRoot
+        echo " Configuration file $wslcfg has been modified. A backup of the old file
  can be found in folder $PSScriptRoot"
+    }
+    if ($WSL2line -eq 0) {
+        # There isn't a WSL2 section
+        "" >> $wslcfg
+        "[wsl2]" >> $wslcfg
+        "$newline" >> $wslcfg
+    }
+    elseif ($WSL2line -gt $Krnlline) {
+        # no kernel line in WSL2 section
+        "" >> $wslcfg
+        "$newline" >> $wslcfg
+    }
+    else {
+        # Replace current custom kernel
+        (gc $wslcfg) -Replace "^ *kernel *=.*", "$newline" | Set-Content $wslcfg
+    }
+}
+install_krnl
