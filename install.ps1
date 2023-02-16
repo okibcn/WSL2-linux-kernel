ddb53071914e6fa600050cd7eb7eafc000ee5416
Mioki <22417711+okibcn@users.noreply.github.com>
2023-02-16 11:35:58 -0800

    save state

--- install.ps1
+++ install.ps1
@@ -1,6 +1,17 @@
+cd $PSScriptRoot
+$kernel=(gci WSLk*).PSPath
+$line="kernel=$($kernel -replace '/|\\','\\')"
 $wslcfg="~/.wslconfig"
-if (gc $wslcfg | sls "^ *kernel *=") {
-    "We already have a kernel setting"
+cp $wslcfg $PSScriptRoot
+if ( (Test-Path $wslcfg) -AND (gc $wslcfg | sls "^ *kernel *=") ) {
+    # We already have a kernel setting
+    (gc $wslcfg) -Replace "^ *kernel *=.*","$line" | Set-Content $wslcfg
 } else {
-    "no kernel setting yet"
+    if ( !( (Test-Path $wslcfg) -AND (gc $wslcfg | sls "^ *[WSL2]") ) ){
+        # We don't have a [WSL2] section
+        "`n[WSL2]" >> $wslcfg
+    } 
+    "'n$line" >> $wslcfg
 }
+echo " Configuration file $wslcfg has been modified. A backup of the old file 
+ can be found in folder $PSScriptRoot"
